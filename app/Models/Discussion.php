bb8a9f12568695a187bda6dbda8c177e4b8fd0f6
vinnto <vintorizalfi0601@gmail.com>
2025-10-07 20:21:53 +0700

    Setup Discussion and Reply models with relationships

--- app/Models/Discussion.php
+++ app/Models/Discussion.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Discussion extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'course_id',
+        'user_id',
+        'content'
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function replies()
+    {
+        return $this->hasMany(Reply::class);
+    }
 }
--- app/Models/Reply.php
+++ app/Models/Reply.php
@@ -3,8 +3,25 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Reply extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'discussion_id',
+        'user_id',
+        'content'
+    ];
+
+    public function discussion()
+    {
+        return $this->belongsTo(Discussion::class);
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
