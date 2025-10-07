b0a143125e17f5338f17874a59724841183342ed
vinnto <vintorizalfi0601@gmail.com>
2025-10-07 16:44:01 +0700

    Setup Assignment and Submission models with relationships

--- app/Models/Assignment.php
+++ app/Models/Assignment.php
@@ -3,8 +3,30 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Assignment extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'course_id',
+        'title',
+        'description',
+        'deadline'
+    ];
+
+    protected $casts = [
+        'deadline' => 'datetime'
+    ];
+
+    public function course()
+    {
+        return $this->belongsTo(Course::class);
+    }
+
+    public function submissions()
+    {
+        return $this->hasMany(Submission::class);
+    }
 }
