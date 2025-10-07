1815367b53b08c2720f6dcabf104f4eefcee8b74
vinnto <vintorizalfi0601@gmail.com>
2025-10-07 13:49:05 +0700

    Setup API routes for courses management

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CourseController;
 use App\Http\Controllers\API\AuthController;
 
 Route::get('/user', function (Request $request) {
@@ -15,4 +16,9 @@ Route::post('/login', [AuthController::class, 'login']);
 // Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::post('/courses', [CourseController::class, 'store'])->middleware('role:dosen');
+    Route::put('/courses/{id}', [CourseController::class, 'update'])->middleware('role:dosen');
+    Route::delete('/courses/{id}', [CourseController::class, 'destroy'])->middleware('role:dosen');
+    Route::post('/courses/{id}/enroll', [CourseController::class, 'enroll'])->middleware('role:mahasiswa');
 });
